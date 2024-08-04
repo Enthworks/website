43d5c80ad133900892b2d10bb0303d287e270326
Enthworks <enthworks@gmail.com>
2024-08-04 16:25:53 +0530

    Website files
    Second commit

---
+++ contactform.php
@@ -0,0 +1,32 @@
+<?php
+// Get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$phone= $_POST['phone'];
+$project= $_POST['project'];
+$subject= $_POST['subject'];
+$message= $_POST['message'];
+ 
+$to = "enthworks@gmail.com";
+$subject = "Website Enquiry";
+ 
+// The following text will be sent
+// Name = user entered name
+// Email = user entered email
+// Message = user entered message 
+$txt ="Name = ". $name . "\r\n  Email = "
+    . $email . "\r\n Phone =" . $phone. 
+	"\r\n Company =" . $project .
+	"\r\n Subject =" . $subject . 
+	"\r\n Message =" . $message;
+ 
+$headers = "From: noreply@enthworks.com" . "\r\n" .
+           "CC: somebodyelse@example.com";
+		   
+if($email != NULL) {
+    mail($to, $subject, $txt, $headers);
+}
+ 
+// Redirect to
+header("Location:contact.html");
+?>
